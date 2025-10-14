aa78a75bf914f8e8370eeceb986464c4358fb53c
rubenscarius <135164952+rubenscarius@users.noreply.github.com>
2025-10-14 20:12:37 -0300

    add function call back xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -11,57 +11,78 @@ int main() {
     int torre = 5;
     int bispo = 5;
     int rainha = 8;
-    int count1 = 1;
-    int count2 = 1;
-    int cavalo1 = 2;
-    int cavalo2 = 1;
+    
+    // Realiza os movimentos da torre usando a estrutura de loop while em uma função
+    void movimentacaoTorre(contador, torre) {
+      while (contador <= torre) {
+        printf("Direita\n");
+      
+        contador++;
+      }
+    }
+    
+    // Realiza os movimentos da torre usando recursividade
+    void movimentacaoRecursivaTorre(contador, torre) {
+      printf("Direita\n");
+      if (contador < torre) {
+        contador++;
+        movimentacaoRecursivaTorre(contador, torre);
+      }
+    }
+    
+    // Realiza os movimentos da rainha usando a estrutura de loop for
+    void movimentacaoRainha(contador, rainha) {
+      for (contador; contador <= rainha; contador++) {
+      printf("Esquerda\n");
+      }
+    }
+    
+    // Realiza os movimentos do bispo usando a estrutura de loop do while
+    void movimentacaoBispo(contador, bispo) {
+      do {
+      printf("Cima, Direita\n");
+      
+      contador++;
+      } while (contador <= bispo);
+    }
+    
+    void movimentacaoCavalo() {
+      int count1 = 1;
+      int count2 = 1;
+      int cavalo1 = 2;
+      int cavalo2 = 1;
+      
+      for (count1; count1 <= cavalo2; count1++) {
+        for (count2; count2 <= cavalo1; count2++) {
+         printf("Baixo\n");
+        }
+        printf("Esquerda");
+      }
+    }
     
     printf("Movimento de algumas peças de Xadrez.\n");
     printf("=====================================\n");
     printf("Movimento da Torre\n");
     printf("-------------------------------------\n");
-    
-    // Realiza os movimentos da torre usando a estrutura de loop while
-    while (contador <= torre) {
-      printf("Direita\n");
-      
-      contador++;
-    }
-    contador = 1;
+    movimentacaoRecursivaTorre(contador, torre);
     
     printf("\n");
     printf("Movimento do Bispo\n");
     printf("-------------------------------------\n");
-    
-    // Realiza os movimentos do bispo usando a estrutura de loop do while
-    do {
-      printf("Cima, Direita\n");
-      
-      contador++;
-    } while (contador <= bispo);
-    contador = 1;
+    movimentacaoBispo(contador, bispo);
     
     printf("\n");
     printf("Movimento da Rainha\n");
     printf("-------------------------------------\n");
+    movimentacaoRainha(contador, rainha);
     
-    // Realiza os movimentos da rainha usando a estrutura de loop for
-    for (contador; contador <= rainha; contador++) {
-      printf("Esquerda\n");
-    }
-    contador = 1;
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("\n");
     printf("Movimentação do Cavalo\n");
     printf("-------------------------------------\n");
-    
-    for (count1; count1 <= cavalo2; count1++) {
-      for (count2; count2 <= cavalo1; count2++) {
-        printf("Baixo\n");
-      }
-      printf("Esquerda");
-    }
+    movimentacaoCavalo();
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
